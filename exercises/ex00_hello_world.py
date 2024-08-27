5a2f6c9306a123d3c8b1e61cd6dd91c65dbb9722
nandanpu <nandanpu@unc.edu>
2024-08-26 21:45:29 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,7 @@
+"""My first excercise in COMP110!"""
+
+__author__ = "730768561"
+
+def greet(name: str) -> str: 
+    """A Welcoming first function definition."""
+    return "Hello, " + name + "!"
--- exercises/ex07/fish.py
+++ exercises/ex07/fish.py
@@ -1,9 +1,6 @@
 """File to define Fish class."""
-
 class Fish:
-    
     def __init__(self):
         return None
-    
     def one_day(self):
         return None
